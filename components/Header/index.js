90edf8cdc30a515307195e10bf001e16e335b7ab
Neha Patel <neha.r.p@gmail.com>
2019-07-12 08:47:46 -0700

    fixed typo in header/index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,6 +7,8 @@
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
-// And add it to the DOM in the .headerContainer component
+// And add it to the DOM in the .header-Container component
 
-function Header() {}
+function Header() {
+
+}
